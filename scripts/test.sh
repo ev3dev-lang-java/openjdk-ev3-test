616c860c187876bb23bcb4750e6f6ae80da0d27e
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-05-28 09:55:25 +0200

    Adding the whole logic

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-ls /
-ls /home/
+cd /home/compiler/
+wget https://ci.adoptopenjdk.net/view/ev3dev/job/openjdk-10-ev3/lastSuccessfulBuild/artifact/build/jdk-ev3.tar.gz
+tar -xf jdk-ev3.tar.gz
+update-alternatives --install /usr/bin/java java /home/robot/jdk/bin/java 2000
+java -version
+apt-get update
+apt-get install --yes --no-install-recommends apt-utils
+apt-get install --yes --no-install-recommends build-essential
+apt-get install --yes --no-install-recommends git
+apt-get install --yes --no-install-recommends ant
+git clone https://github.com/AdoptOpenJDK/openjdk-tests.git
+cd openjdk-tests
+cpan install JSON Text::CSV
+export JAVA_BIN=/home/compiler/jdk/bin/
+export SPEC=linux_arm
+export JAVA_VERSION=SE100
+cd TestConfig
+./get.sh   -t /home/compiler/openjdk-tests   -p   linux_arm   -v    openjdk10
+make -f run_configure.mk
+make compile
+make sanity
