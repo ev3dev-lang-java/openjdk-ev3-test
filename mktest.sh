2964f4e0fea0dd1b2d756fa4196c3eb81bb9e082
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-05-28 16:37:43 +0200

    Complete logic

--- mktest.sh
+++ mktest.sh
@@ -1,3 +1,22 @@
 #/bin/bash
+set -e
 
 echo "Running Script to test OpenJDK"
+
+wget https://ci.adoptopenjdk.net/view/ev3dev/job/openjdk-10-ev3/lastSuccessfulBuild/artifact/build/jdk-ev3.tar.gz
+tar -xf jdk-ev3.tar.gz
+sudo update-alternatives --install /usr/bin/java java /home/compiler/jdk/bin/java 2000
+java -version
+exit 0
+
+git clone https://github.com/AdoptOpenJDK/openjdk-tests.git
+cd openjdk-tests
+export BUILD_LIST=openjdk_regression
+export JAVA_BIN=/home/compiler/jdk/bin/
+export SPEC=linux_arm
+export JAVA_VERSION=SE100
+cd TestConfig
+./get.sh   -t /home/compiler/openjdk-tests   -p   linux_arm   -v    openjdk10
+make -f run_configure.mk
+make compile
+make sanity
