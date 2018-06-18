4359ec426bba3463f603e325038459b672606291
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-06-18 10:50:08 +0200

    Enabling jdk_security1

--- mktest.sh
+++ mktest.sh
@@ -56,8 +56,8 @@ function run_tests() {
     #make jdk_beans
     #make jdk_other
     #make jdk_net
-    make jdk_nio
-    #make jdk_security1
+    #make jdk_nio
+    make jdk_security1
     #make jdk_security2
     #make jdk_security3
     #make jdk_text
