93d88b65ec7a7085332ee6f9d56104f7ab12abd7
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-06-21 22:25:04 +0200

    enabling jdk_text

--- mktest.sh
+++ mktest.sh
@@ -59,8 +59,8 @@ function run_tests() {
     #make jdk_nio
     #make jdk_security1
     #make jdk_security2
-    make jdk_security3
-    #make jdk_text
+    #make jdk_security3
+    make jdk_text
     #make jdk_util
     #make jdk_time
     #make jdk_management
